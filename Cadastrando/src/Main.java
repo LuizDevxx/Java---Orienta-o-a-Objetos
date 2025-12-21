4f12feaddbea7d730648a9d107e715f31a7aa66e
Luiz Ferreira <luiz.jesusferreira2003@gmail.com>
2025-12-21 00:23:35 -0300

    Add files via upload

---
+++ Cadastrando/src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner e = new Scanner(System.in);
+        PessoaService p = new PessoaService();
+
+        while (true) {
+            System.out.println("Digite a opção que deseja: ");
+            System.out.println("1- Cadastrar Pessoa");
+            System.out.println("2- Listar pessoas");
+            System.out.println("3- Buscar por id");
+            System.out.println("4- Remover usuario");
+            System.out.println("5- Sair");
+            System.out.print("Opção: ");
+            int opcao = e.nextInt();
+
+            System.out.println();
+
+
+            switch (opcao) {
+                case 1 -> {
+                    p.cadastrar();
+                }
+                case 2 -> {
+                    p.listar();
+                }
+                case 3 -> {
+                    p.buscar();
+                }
+                case 4 -> {
+                    p.excluir();
+                }
+                case 5 -> {
+                    System.out.println("Obrigado por usar nosso sistema");
+                    return;
+                }
+                default -> {
+
+                }
+            }
+        }
+    }
+}
